dd3402c173486cf04fecaa46d5827b4303990f68
Daria <greta.von.stein@gmail.com>
2019-07-22 19:14:57 +0200

    June changes

--- main.go
+++ main.go
@@ -1,99 +1,68 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
-	"os/signal"
+	"path/filepath"
 	"runtime"
-	"runtime/pprof"
+	"runtime/trace"
+	"strings"
 	"sync"
-	"syscall"
-	"time"
 )
 
+const size = 1000
+
+type tracker struct {
+	sync.Mutex
+	m map[string]struct{}
+}
+
+var t tracker
+
 func main() {
-	pprof.StartCPUProfile(os.Stdout)
-	defer pprof.StopCPUProfile()
+	runtime.GOMAXPROCS(4)
 
-	// Below is an example of using our PrintMemUsage() function
-	// Print our starting memory usage (should be around 0mb)
-	PrintMemUsage()
+	trace.Start(os.Stdout)
+	defer trace.Stop()
 
-	finish := make(chan struct{})
-	go listenForSignals(finish)
+	dir := "/Users/dariakameneva/gopath/src/"
+	t.m = make(map[string]struct{})
 
-	var overall [][]int
-	defer func() {
-		// Clear our memory and print usage, unless the GC has run 'Alloc' will remain the same
-		overall = nil
-		PrintMemUsage()
-
-		// Force GC to clear up, should see a memory drop
-		runtime.GC()
-		PrintMemUsage()
-	}()
-
-	var counter int
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func(done <-chan struct{}) {
-		defer wg.Done()
-		for {
-			select {
-			case <-done:
-				return
-			default:
-			}
-			// Allocate memory using make() and append to overall (so it doesn't get
-			// garbage collected). This is to create an ever increasing memory usage
-			// which we can track. We're just using []int as an example.
-			a := make([]int, 0, 999999)
-			overall = append(overall, a)
-
-			// Print our memory usage at each interval
-			PrintMemUsage()
-			time.Sleep(time.Second)
-
-			if counter%10 == 0 {
-				overall = nil
-			}
-
-			counter++
-		}
-	}(finish)
-
-	wg.Wait()
-}
-
-// PrintMemUsage outputs the current, total and OS memory being used. As well as the number
-// of garage collection cycles completed.
-func PrintMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-}
-
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
-}
-
-func listenForSignals(done chan<- struct{}) {
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
-	for sig := range signalChan {
-		switch sig {
-		default:
-			log.Panicf("Unexpected signal: %v", sig)
-
-		case syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
-			close(done)
-		}
+	if err := filepath.Walk(dir, walk); err != nil {
+		log.Fatal(err)
 	}
+}
+
+func walk(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+
+	if info.IsDir() {
+		return nil
+	}
+
+	go func() {
+		file, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		content, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		for _, l := range strings.Split(string(content), "\n") {
+			if strings.Contains(l, "go func") {
+				t.Lock()
+				t.m[filepath.Base(path)] = struct{}{}
+				t.Unlock()
+			}
+		}
+	}()
+	return nil
 }
